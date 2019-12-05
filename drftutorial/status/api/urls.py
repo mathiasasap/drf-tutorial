86a0bed76777c90633cbf1725b1103c67c554d5f
mathias <mathias@asapdevelopers.com>
2019-12-05 17:02:54 -0300

    update and delete api view

--- drftutorial/status/api/urls.py
+++ drftutorial/status/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
 from .views import StatusListSearchAPIView
-from .views import StatusAPIView, StatusCreateAPIView, StatusDetailAPIView
+from .views import StatusAPIView, StatusCreateAPIView, StatusDetailAPIView, StatusUpdateAPIView, StatusDeleteAPIView
 
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
     url(r'^create/$', StatusCreateAPIView.as_view()),
-    url(r'^(?P<pk>.*)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/update/$', StatusUpdateAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/delete/$', StatusDeleteAPIView.as_view()),
 ]
--- drftutorial/status/api/views.py
+++ drftutorial/status/api/views.py
@@ -58,6 +58,20 @@ class StatusDetailAPIView(generics.RetrieveAPIView):
     #     return Status.objects.get(id=kw_id)
 
 
+class StatusUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id' # To define which url argument use
+
+
+class StatusDeleteAPIView(generics.DestroyAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id' # To define which url argument use
 
 
 
