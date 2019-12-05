6b6e4a086f366dd673fefb0ddcf6b4ae5e4a0fa4
mathias <mathias@asapdevelopers.com>
2019-12-05 16:52:53 -0300

    Added create view

--- drftutorial/status/api/urls.py
+++ drftutorial/status/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from .views import StatusListSearchAPIView
-from .views import StatusAPIView
+from .views import StatusAPIView, StatusCreateAPIView
 
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
 ]
