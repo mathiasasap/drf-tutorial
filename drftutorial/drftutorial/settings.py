99098808a6516858c6661258d33f15d6a1b942b6
mathias <mathias@asapdevelopers.com>
2019-12-11 14:04:38 -0300

    Add upload images

--- drftutorial/drftutorial/settings.py
+++ drftutorial/drftutorial/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-server', 'media_root')
+MEDIA_URL = '/media/'
--- drftutorial/status/models.py
+++ drftutorial/status/models.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.db import models
 
+
 def upload_status_image(instance, filename):
-    return "updates/{user}/{filename}".format(user=instance.user, filename=filaname)
+    return "status/{user}/{filename}".format(user=instance.user, filename=filename)
 
 
 class StatusQuerySet(models.QuerySet):
