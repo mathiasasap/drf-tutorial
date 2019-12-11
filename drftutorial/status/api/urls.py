6dcc12813547f23ca21ca6b9854669b9662fb6d8
mathias <mathias@asapdevelopers.com>
2019-12-11 11:34:28 -0300

    Implemented the CRUD in one endpoint

--- drftutorial/status/api/urls.py
+++ drftutorial/status/api/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 
-from .views import StatusListSearchAPIView
-from .views import StatusAPIView, StatusDetailAPIView
+from .views import StatusAPIView
 
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
-    url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
+    # url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
 ]
--- drftutorial/status/api/views.py
+++ drftutorial/status/api/views.py
@@ -1,31 +1,38 @@
 from rest_framework import generics, mixins
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from status.models import Status
 from .serializers import StatusSerializer
 
 
-class StatusListSearchAPIView(APIView):
-    permission_classes = []
-    authentication_classes = []
-
-    def get(self, request, format=None):
-        qs = Status.objects.all()
-        serializer = StatusSerializer(qs, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        qs = Status.objects.all()
-        serializer = StatusSerializer(qs, many=True)
-        return Response(serializer.data)
+# class StatusListSearchAPIView(APIView):
+#     permission_classes = []
+#     authentication_classes = []
+#
+#     def get(self, request, format=None):
+#         qs = Status.objects.all()
+#         serializer = StatusSerializer(qs, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         qs = Status.objects.all()
+#         serializer = StatusSerializer(qs, many=True)
+#         return Response(serializer.data)
 
 
 # CreateModelMixin --- post data
 # UpdateModelMixin --- put data
 # DestroyModelMixin --- delete data
 
-class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+class StatusAPIView(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.ListAPIView
+):
     permission_classes = []
     authentication_classes = []
     queryset = Status.objects.all()
@@ -38,19 +45,46 @@ class StatusAPIView(mixins.CreateModelMixin, generics.ListAPIView):
             qs = qs.filter(content__icontains=query)
         return qs
 
+    def get_object(self):
+        request = self.request
+        passed_id = request.GET.get('id')
+        qs = self.get_queryset()
+        obj = None
+        if passed_id is not None:
+            obj = get_object_or_404(qs, id=passed_id)
+            self.check_object_permissions(request, obj)
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        passed_id = request.GET.get('id')
+        # request.body
+        # request.data
+        if passed_id is not None:
+            return self.retrieve(self, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
+
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, *kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
     # def perform_create(self, serializer):
     #     serializer.save(user=self.request.user)
 
 
 # Equivalent to the bellow class
-class StatusDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = []
-    authentication_classes = []
-    queryset = Status.objects.all()
-    serializer_class = StatusSerializer
+# class StatusDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = []
+#     authentication_classes = []
+#     queryset = Status.objects.all()
+#     serializer_class = StatusSerializer
 
 # class StatusDetailAPIView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
 #     permission_classes = []
