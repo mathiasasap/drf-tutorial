cdd40745f66952e13774194ce0fda2e4d06e55c4
mathias <mathias@asapdevelopers.com>
2019-12-16 12:43:20 -0300

    Using JWT to manage authentication

--- drftutorial/drftutorial/urls.py
+++ drftutorial/drftutorial/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token # accounts app
+
 from status.api import urls as api_status_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/status/', include(api_status_urls)),
+    path('api/auth/jwt/', obtain_jwt_token),
+    path('api/auth/jwt/refresh', refresh_jwt_token),
 ]
